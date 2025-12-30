0607f6fdaf0ba6bf725f38ef998e27be65c1872d
Roshni <roshnisahoo04@gmail.com>
2025-12-30 20:01:21 +0530

    backend api file edited

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "https://weatherly-5vri.onrender.com"
+  baseURL: "https://weatherly-5vri.onrender.com/api"
 })
 
 // Attach token automatically
